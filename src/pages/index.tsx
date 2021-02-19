e5ab108eb075da24983a39a4588a7e279a6f4037
straxico <straxico@gmail.com>
2021-02-19 19:18:52 +0330

    add typescript

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react"
+import Alert from "@kiwicom/orbit-components/lib/Alert";
+
+
+
+const IndexPage = () => {
+  return (
+    <div><Alert>hiiiiiiiii</Alert></div>
+
+  )
+}
+
+export default IndexPage
