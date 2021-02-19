da1711eb7b6aa15a7030ac9b31e5f5086707aca0
straxico <straxico@gmail.com>
2021-02-19 19:47:43 +0330

    add material design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,16 @@
 import * as React from "react"
 import Heading from "@kiwicom/orbit-components/lib/Heading";
 import Layout, { LayoutColumn } from "@kiwicom/orbit-components/lib/Layout";
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
 
 
 
 const IndexPage = () => {
   return (
-    <Layout type="MMB">
-      <LayoutColumn><Heading>tyto</Heading></LayoutColumn>
-    </Layout>
+    <Container maxWidth="lg">
+<Heading>tyto</Heading><Button variant="contained" color="primary">hi</Button>      </Container>
+
 
   )
 }
